e123f3f51797d8f63ea95152799c3c66e893cd22
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-27 10:22:39 -0500

    dev : commit init

---
+++ src/main/java/com/atorres/nttdata/gatewaymsf/GatewayMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.gatewaymsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/gatewaymsf/GatewayMsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.gatewaymsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
